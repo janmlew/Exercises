6ec65f4fe62d27e89f89834a1f3150b2367457f5
janmlew <janmikolajlewinski@gmail.com>
2024-07-23 08:04:39 +0200

    Basic operations + some project files.

--- main.py
+++ main.py
@@ -8,5 +8,9 @@ spam = 1  # This is a comment too, but the spam thing is not a comment.
 text = "# Hash inside quotes is not considered to be a comment."
 
 print(50 - 6 * 5)
-print((50 - 2*5)/5)  # A division always returns a floating point number.
-print((50 - 2*5)//6)  # But the floor division returns an int.
+print((50 - 2 * 5) / 5)  # A division always returns a floating point number.
+print((50 - 2 * 5) // 6)  # But the floor division returns an int.
+print((50 - 2 * 5) % 6)  # Percent operator returns the remainder of the division.
+print(6 * 6 + 4)  # This returns the original number before division.
+
+print(f"2 squared = {2 ** 2}")
