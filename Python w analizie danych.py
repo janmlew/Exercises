77d41d53cd074ea6acbacddb0833bbecefbdbe84
janmlew <janmikolajlewinski@gmail.com>
2024-10-29 09:34:04 +0100

    Added more exercises from the book.

--- Python w analizie danych.py
+++ Python w analizie danych.py
@@ -29,3 +29,34 @@ print(a is not c)
 print(a == c)
 a = None
 print(a is None)
+
+# Numerical data types
+ival = 17239871
+print(ival**6)
+
+fval = 7.243
+fval2 = 6.78e-5
+print(fval2)
+print(3/2)
+print(3//2)
+
+# Strings
+a = 'one method of defining string'
+b = "another method"
+c = """
+This is a long string of characters,
+one which consists of several lines.
+"""
+print(c.count('\n'))
+print(a[10])
+b = a.replace("string", "a longer string")
+print(b)
+
+a = 5.6
+s = str(a)
+print(list(s))
+print('12\\34')
+print(r'text\text\text\text')  # r = raw text
+print('first part ' + 'second part')
+
+
